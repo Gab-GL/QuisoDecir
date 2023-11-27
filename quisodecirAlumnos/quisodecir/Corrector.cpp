e42890fcd484f5ac88615494f33c51651459b350
Gab-GL <gabriel.gonzalez.02005@gmail.com>
2023-11-27 16:40:40 -0600

    Se elminaron los comentarios innecesarios en las funciones

--- quisodecirAlumnos/quisodecir/Corrector.cpp
+++ quisodecirAlumnos/quisodecir/Corrector.cpp
@@ -15,17 +15,6 @@
 #include "corrector.h"
 #define DEPURAR 1
 
-
-
-
-//Funciones publicas del proyecto
-/*****************************************************************************************************************
-	DICCIONARIO: Esta funcion crea el diccionario completo
-	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario	
-	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
-	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
-	int &	iNumElementos			:	Numero de elementos en el diccionario
-******************************************************************************************************************/
 void	Diccionario			(char *szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int &iNumElementos)
 {
 
@@ -41,22 +30,22 @@ void	Diccionario			(char *szNombre, char szPalabras[][TAMTOKEN], int iEstadistic
 		while ((linea = fgetc(fpDicc)) != EOF) {
 			if ((linea == ' ' || linea == '\n' || linea == '\t' || linea == '\r' || linea == '.' || linea == ';' || linea == '(' || linea == ')' || linea == ',' || linea == '\0')) {
 
-				bool flag = true;
+				int f = 1;
 				if (indicePD != 0) {
 					palabraDetectada[indicePD] = '\0';
-					for (int pd = 0; pd <= iNumElementos; pd++) {
+					for (int pd = 0; pd < iNumElementos; pd++) {
 						if (strcmp(palabraDetectada, szPalabras[pd]) == 0) {
-							flag = false;
+							f = 0;
 							indicePD = 0;
 							iEstadisticas[pd]++;
 							break;
 
 						}
 					}
-					if (flag) {
+					if (f == 1) {
 						strcpy_s(szPalabras[iNumElementos], sizeof(palabraDetectada), palabraDetectada);
 						iEstadisticas[iNumElementos] = 1;
-						/*	printf("\np: %s", palabraDetectada);*/
+						
 						indicePD = 0;
 						iNumElementos++;
 					}
@@ -104,20 +93,6 @@ void	Diccionario			(char *szNombre, char szPalabras[][TAMTOKEN], int iEstadistic
 }
 
 
-
-
-/*****************************************************************************************************************
-	ListaCandidatas: Esta funcion recupera desde el diccionario las palabras validas y su peso
-	Regresa las palabras ordenadas por su peso
-	char	szPalabrasSugeridas[][TAMTOKEN],	//Lista de palabras clonadas
-	int		iNumSugeridas,						//Lista de palabras clonadas
-	char	szPalabras[][TAMTOKEN],				//Lista de palabras del diccionario
-	int		iEstadisticas[],					//Lista de las frecuencias de las palabras
-	int		iNumElementos,						//Numero de elementos en el diccionario
-	char	szListaFinal[][TAMTOKEN],			//Lista final de palabras a sugerir
-	int		iPeso[],							//Peso de las palabras en la lista final
-	int &	iNumLista)							//Numero de elementos en la szListaFinal
-******************************************************************************************************************/
 void	ListaCandidatas		(
 	char	szPalabrasSugeridas[][TAMTOKEN],	//Lista de palabras clonadas
 	int		iNumSugeridas,						//Lista de palabras clonadas
@@ -128,26 +103,20 @@ void	ListaCandidatas		(
 	int		iPeso[],							//Peso de las palabras en la lista final
 	int&	iNumLista)							//Numero de elementos en la szListaFinal
 {
-
-	////Sustituya estas lineas por su código
-	//strcpy_s(szListaFinal[0], szPalabrasSugeridas[ 0] ); //la palabra candidata
-	//iPeso[0] = iEstadisticas[0];			// el peso de la palabra candidata
-	//
-	//iNumLista = 1;							//Una sola palabra candidata
 	iNumLista = 0;
 	for (int i = 0; i < iNumSugeridas; i++) {
 		for (int j = 0; j < iNumElementos; j++) {
-			bool flag = true;
+			int f  = 1;
 
 			for (int d = 0; d < iNumElementos; d++) {
 				if (strcmp(szListaFinal[d], szPalabrasSugeridas[i]) == 0)
 				{
-					flag = false;
+					f = 0;
 					break;
 				}
 			}
 			/*Comparar de Lista del diccionario con la Lista de palabras clonadas */
-			if (strcmp(szPalabrasSugeridas[i], szPalabras[j]) == 0 && flag) {
+			if (strcmp(szPalabrasSugeridas[i], szPalabras[j]) == 0 && f == 1) {
 				
 				strcpy_s(szListaFinal[iNumLista], szPalabrasSugeridas[i]);
 				iPeso[iNumLista++] = iEstadisticas[j];
@@ -155,23 +124,6 @@ void	ListaCandidatas		(
 		}
 	}
 
-	////Eliminar duplicados y ordenar las palabras
-	//for (int r = 0; r < iNumLista; r++) {
-	//	for (int j = r + 1; j < iNumLista; j++) {
-	//		if (strcmp(szListaFinal[r], szListaFinal[j]) == 0) {
-	//			/*aumentar las estadisticas de las palabras que se repiten*/
-	//			/*	 Eliminar duplicados moviendo los elementos restantes hacia arriba*/
-	//			for (int k = j; k < iNumLista - 1; k++) {
-	//				strcpy_s(szListaFinal[k], szListaFinal[k + 1]);
-
-	//			}
-	//			iNumLista--;
-	//			j--;
-	//		}
-
-	//	}
-	//}
-	//
 	for ( int i = 0; i < iNumLista - 1; i++) {
 		for (int j = 0; j < iNumLista - i - 1; j++) {
 			if (iPeso[j] < iPeso[j + 1]) {
@@ -193,18 +145,12 @@ void	ListaCandidatas		(
 
 /*****************************************************************************************************************
 	ClonaPalabras: toma una palabra y obtiene todas las combinaciones y permutaciones requeridas por el metodo
-	char *	szPalabraLeida,						// Palabra a clonar
-	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
-	int &	iNumSugeridas)						//Numero de elementos en la lista
 ******************************************************************************************************************/
 void	ClonaPalabras(
 	char* szPalabraLeida,						// Palabra a clonar
 	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
 	int& iNumSugeridas)						//Numero de elementos en la lista
 {
-	//Sustituya estas lineas por su código
-	//strcpy(szPalabrasSugeridas[0], szPalabraLeida); //lo que sea que se capture, es sugerencia
-								//Una sola palabra sugerida
 	iNumSugeridas = 0;
 	char abcd[] = "abcdefghijklmnñopqrstuvwxyzáéíóú";
 	char auxclonada[TAMTOKEN];
@@ -267,12 +213,11 @@ void	ClonaPalabras(
 	
 
 	
-	for (int v = 0; v < iNumSugeridas - 1  ; v++) {
+	for (int v = 0; v < iNumSugeridas - 1 ; v++) {
 
-		for (int b = v + 1; b < iNumSugeridas - v - 1; b++) {
+		for (int b = v + 1; b < iNumSugeridas; b++) {
 
 			if (strcmp(szPalabrasSugeridas[v], szPalabrasSugeridas[b]) > 0) {
-				/*Lista de palabras clonadas al aux*/
 				strcpy_s(auxclonada, szPalabrasSugeridas[v]);
 				strcpy_s(szPalabrasSugeridas[v], szPalabrasSugeridas[b]);
 				strcpy_s(szPalabrasSugeridas[b], auxclonada);
@@ -280,3 +225,4 @@ void	ClonaPalabras(
 		}
 	}
 }
+
